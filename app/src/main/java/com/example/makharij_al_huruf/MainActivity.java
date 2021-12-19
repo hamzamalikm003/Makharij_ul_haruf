2a9bd8a43dfd3b765290838dd012500f5cbf62e0
Hamza Malik <hamza.malik@frag-games.com>
2021-12-19 18:04:24 +0500

    Appbar in app

--- app/src/main/java/com/example/makharij_al_huruf/MainActivity.java
+++ app/src/main/java/com/example/makharij_al_huruf/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.makharij_al_huruf;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -23,11 +26,14 @@ public class MainActivity extends AppCompatActivity implements
     private ImageView imageView;
     private Button Ghunna;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initialize();
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
         Halqiyah.setOnClickListener(this);
 //        Halqiyah.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -172,5 +178,87 @@ public class MainActivity extends AppCompatActivity implements
         Ghunna = findViewById(R.id.Ghunna);
         imageView = findViewById(R.id.imageView);
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_halqiyah:
+
+                Intent newintent = new Intent(MainActivity.this, practice.class);
+                newintent.putExtra("button", "Halqiyah");
+                startActivity(newintent);
+                return true;
+
+
+
+            case R.id.action_lahatiyah:
+
+                Intent newintent1 = new Intent(MainActivity.this, practice.class);
+                newintent1.putExtra("button", "Lahatiyah");
+                startActivity(newintent1);
+                return true;
+
+
+
+            case R.id.action_shajariyah:
+
+                Intent newintent2 = new Intent(MainActivity.this, practice.class);
+                newintent2.putExtra("button", "Shajariyah");
+                startActivity(newintent2);
+                return true;
+
+
+
+            case R.id.action_tarfiyah:
+
+                Intent newintent3 = new Intent(MainActivity.this, practice.class);
+                newintent3.putExtra("button", "Tarfiyah");
+                startActivity(newintent3);
+                return true;
+
+
+
+            case R.id.action_nitEeyah:
+
+                Intent newintent4 = new Intent(MainActivity.this, practice.class);
+                newintent4.putExtra("button", "Niteeyah");
+                startActivity(newintent4);
+                return true;
+
+
+
+            case R.id.action_lisaveyah:
+
+                Intent newintent5 = new Intent(MainActivity.this, practice.class);
+                newintent5.putExtra("button", "Lisaveyah");
+                startActivity(newintent5);
+                return true;
+
+
+
+            case R.id.action_ghunna:
+
+                Intent newintent6 = new Intent(MainActivity.this, practice.class);
+                newintent6.putExtra("button", "Ghunna");
+                startActivity(newintent6);
+                return true;
+
+
+
+
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
